7da0fd7dcb7637ad560ab79c2312e9b99aa23506
RichardL007 <132619629+RichardL007@users.noreply.github.com>
2024-11-29 22:54:23 +0000

    Initial config complete

--- A320Clock/A320Clock.h
+++ A320Clock/A320Clock.h
@@ -2,10 +2,10 @@
 
 #include "Arduino.h"
 
-class MyCustomClass
+class A320Clock
 {
 public:
-    MyCustomClass(uint8_t Pin1, uint8_t Pin2);
+    A320Clock(uint8_t Pin1, uint8_t Pin2);
     void begin();
     void attach(uint16_t Pin3, char *init);
     void detach();
